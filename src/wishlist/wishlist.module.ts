6434d7ba6e5ae12f259ba1d5aee1992ebc763963
Dimitri Denis <dimitri.denis@etu.unilim.fr>
2022-02-03 21:02:28 +0000

    Third commit

--- src/wishlist/wishlist.module.ts
+++ src/wishlist/wishlist.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { WishlistService } from './wishlist.service';
 import { WishlistResolver } from './wishlist.resolver';
+import { WishlistSchema } from './schema/wishlist.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [MongooseModule.forFeature(({name: 'Wishlist', schema: WishlistSchema}))],
   providers: [WishlistResolver, WishlistService]
 })
 export class WishlistModule {}
--- src/wishlist/wishlist.service.ts
+++ src/wishlist/wishlist.service.ts
@@ -2,25 +2,33 @@ import { Injectable } from '@nestjs/common';
 import { CreateWishlistInput } from './dto/create-wishlist.input';
 import { UpdateWishlistInput } from './dto/update-wishlist.input';
 
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Wishlist } from './schema/wishlist.schema';
+
 @Injectable()
 export class WishlistService {
-  create(createWishlistInput: CreateWishlistInput) {
-    return 'This action adds a new wishlist';
+
+constructor(@InjectModel('Wishlist') private readonly wishlistModel: Model<Wishlist>){}
+
+  async create(createWishlistInput: CreateWishlistInput): Promise <Wishlist> {
+    const newWishlist = await new this.wishlistModel(createWishlistInput);
+    return newWishlist.save();
   }
 
-  findAll() {
-    return `This action returns all wishlist`;
+   async findAll(): Promise <Wishlist> {
+    return this.wishlistModel.find().exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} wishlist`;
+  async findOne(id: string) {
+    return this.wishlistModel.findOne().exec();
   }
 
-  update(id: number, updateWishlistInput: UpdateWishlistInput) {
+  update(id: string, updateWishlistInput: UpdateWishlistInput) {
     return `This action updates a #${id} wishlist`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} wishlist`;
+  remove(id: string) {
+    return this.wishlistModel.findOneAndRemove().exec();
   }
 }
